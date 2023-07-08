b79ab2e9da06995654c9f4ed85748197df493884
Vectorlegend <snd778g@gmail.com>
2023-07-08 12:41:20 +0530

    init

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById("submitButton").onclick = function () {
+  let temp;
+
+  if (document.getElementById("cButton").checked) {
+    temp = document.getElementById("textbox").value;
+    temp = Number(temp);
+    temp = toCelcius(temp);
+    document.getElementById("templabel").innerHTML = temp + "C";
+  } else if (document.getElementById("fButton").checked) {
+    temp = document.getElementById("textbox").value;
+    temp = Number(temp);
+    temp = toFahrenheit(temp);
+    document.getElementById("templabel").innerHTML = temp + "F";
+  } else {
+    document.getElementById("templabel").innerHTML =
+      "Please select one of the followings";
+  }
+};
+
+function toCelcius(temp) {
+  return (temp - 32) * (5 / 9);
+}
+
+function toFahrenheit(temp) {
+  return temp * (9 / 5) + 32;
+}
